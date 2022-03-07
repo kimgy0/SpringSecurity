ded006f73ded1068f5ed04557592b503791e976a
01086055223@hanmail.net <01086055223@hanmail.net>
2022-03-07 21:21:38 +0900

    [FIX]RememberMe Cust

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -18,10 +20,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@Configuration      // 설정파일이기 때문에 적어줘야한다.
-@EnableWebSecurity  // 이 어노테이션은 @Import({WebSecurityConfiguration.class, SpringWebMvcImportSelector.class, OAuth2ImportSelector.class, HttpSecurityConfiguration.class})
+//@Configuration      // 설정파일이기 때문에 적어줘야한다.
+//@EnableWebSecurity  // 이 어노테이션은 @Import({WebSecurityConfiguration.class, SpringWebMvcImportSelector.class, OAuth2ImportSelector.class, HttpSecurityConfiguration.class})
                     //등과 같이 설정을 해주는 어노테이션이기 때문에 적어줘야한다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -83,5 +87,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .deleteCookies("remember-me");
 
+        //remember-me function
+        http.rememberMe()
+//        쿠키를 가지고 서버에 접근하게 되는것.
+//                세션 만료후 웹브라우저가 종료되어도 만료일을 정해서 쿠키를 가지고 자동로그인 설정하기 같은 기능을 사용한다.
+//        remember me 쿠키를 가지고 서버에 접근하는 경우 서버는 쿠키를 확인하고 토큰 기반의 인증을 하게 된다. 토큰 검증 후 사용자는 로그인을 한다.
+//        세가지 라이프사이클에
+//        - 인증 성공 ( remember-me 쿠키 설정)
+//                - 인증 실패 쿠키가 존재하면 쿠키 무효화한다.
+//                - 로그아웃도 똑같은 역할을 한다.
+                .rememberMeParameter("remember")
+                //rememberMe 기능을 이용할 체크박스의 name 속성
+                .tokenValiditySeconds(3600)
+                //디폴트는 14일동안 유지 가능하다.
+                .alwaysRemember(false)
+                //리멤버 기능이 활성화 되어지지 않아도(체크박스에 설정되어지지 않아도)
+                //true 를 성정하면 로그인이 되는 순간 rememberMe 기능이 활성
+                .userDetailsService(userDetailsService);
+                //시스템에 사용자 계정을 조회할 때 쓰는 클래스를 인자로.
+
+        /*
+            기존에 기능을 쓰지 않으면 jsession id를 삭제해주면 다시 로그인해야하는데
+            이기능을 적용하게 되면 jession id 를 삭제해도 쿠키가 존재하기 때문에
+            인증에 성공하게 된다. -> 쿠키에는 username, password, 만료일 등등이 존재한다.
+
+            인증에 성공할 경우 jsessionid 를 가지고 서버가 클라이언트 jsession 를 꺼내고
+            그 세션에 시큐리티 컨텍스트가 있고 그 안에 인증에 성공한 인증객체가 존재.
+            이렇게 매칭 후 스프링이 인증된 객체가 맞는지 확인.
+            클라이언트 쿠키엔 jsessionId 가 존재. -> 서버도 가지고있음 세션아이디를
+         */
     }
 }
